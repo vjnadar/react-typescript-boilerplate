e5580241bd5a20283c0075c09768a8ff9d01bf54
Vijay Joshua Nadar <32163394+vjnadar@users.noreply.github.com>
2024-02-16 19:58:14 +0530

    Testing husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -14,7 +14,7 @@ describe("App.js test", () => {
         const editSection = screen.getByTestId("edit");
         expect(editSection).toHaveTextContent(/Edit src\/App.tsx and save to reload./i);
         await waitFor(() => {
-            const linkElement = screen.getByText("Learn React.");
+            const linkElement = screen.getByText(/learn react./i);
             expect(linkElement).toBeInTheDocument();
         });
     });
